bae9693fbababf29424d052f2e0143f98aacf40c
YingshanGuo <nana_312005071@qq.com>
2017-08-15 09:45:32 +0800

    test

---
+++ js/main.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    console.log("main.js  success!");
+
+}
---
+++ js/mobile-slider.js
@@ -0,0 +1,6 @@
+    // <!--触摸屏效果滑动组件-->
+    <script type="text/javascript" src="./js/common/touch.js"></script>
+    <script type="text/javascript" src="./js/common/zepto.js"></script>
+    <script type="text/javascript" src="./js/common/zepto.ui.js"></script>
+    <script type="text/javascript" src="./js/common/slider.js"></script>
+    // <!--触摸屏效果滑动组件end-->
